942384e48e11a902d7612ff2e664a52d0b1ff8e1
Steven Hildreth <sphildreth@gmail.com>
2015-08-18 22:37:05 -0500

    Changes for path manipulation

--- resources/validator.py
+++ resources/validator.py
@@ -45,7 +45,6 @@ class Validator(ProcessorBase):
                             Track.delete(track.Track)
                         self.logger.warn("X Deleting Track [" + str(track.Track) + "] File [" + trackFilename + "] not found")
                     elif track not in goodTracks:
-                        self.logger.info("Adding Track [" + str(track.Track) + '] Hash [' + track.Hash + '] to Release')
                         goodTracks.append(track)
                 except:
                     pass
