348dcc4e418e042a2147bd6fea0edfa9fab7dffb
sphildreth@gmail.com <gh1980!>
2015-09-05 17:34:20 -0500

    Processing skips locked artists.

--- resources/processor.py
+++ resources/processor.py
@@ -191,6 +191,9 @@ class Processor(ProcessorBase):
                             # If not by alternate name look for match on SortName
                             if not artist:
                                 artist = Artist.objects(SortName__iexact=id3.artist).first()
+                        if artist and artist.IsLocked:
+                            self.logger.debug("Skipping Processing Track [" + mp3 + "], Artist [" + str(artist) + "] Is Locked")
+                            continue
                         if not artist:
                             # Artist not found create
                             artist = Artist(Name=id3.artist)
