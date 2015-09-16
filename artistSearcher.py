4e1ed5dd12a6099ecbc53a30dc720224bc081b43
sphildreth@gmail.com <gh1980!>
2015-09-15 20:30:45 -0500

    Setting up Artist Search engine.

--- artistSearcher.py
+++ artistSearcher.py
@@ -4,17 +4,25 @@ from resources.artistSearcher import ArtistSearcher
 p = argparse.ArgumentParser(description='Search For Artist Information.')
 p.add_argument('--name', '-n', help="Artist Name", required=True)
 p.add_argument('--release', '-r', help="Release Title", required=False)
+p.add_argument('--showMissing', '-s', action='store_true', help="Show Releases Not Found In Roadie Database", required=False)
 args = p.parse_args()
 
-with ArtistSearcher() as s:
+with ArtistSearcher(None) as s:
     artistInfo = s.searchForArtist(args.name)
     if artistInfo:
         print("Artist Info: " + str(artistInfo))
         releases = s.searchForArtistReleases(artistInfo, args.release)
         if releases:
-            print("Artist Releases Count [" + str(len(releases)) + "]")
-            for releases in releases:
-                print("Release Info [" + str(releases) + "]")
+            missing = 0
+            for release in releases:
+                if not release.roadieId or release.roadieId == "None":
+                    missing += 1
+            print("Artist Releases Count [" + str(len(releases)) + "] Missing [" + str(missing) + "]")
+            for release in releases:
+                if args.showMissing and not release.roadieId or release.roadieId == "None":
+                    print("[Missing] Release Info [" + str(release) + "]")
+                elif not args.showMissing:
+                    print("Release Info [" + str(release) + "]")
         else:
             print("No Releases Found!")
     else:
--- resources/artistSearcher.py
+++ resources/artistSearcher.py
@@ -6,6 +6,8 @@ from io import StringIO
 from urllib import request, parse
 from resources.logger import Logger
 from resources.musicBrainz import MusicBrainz
+from mongoengine import connect
+from resources.models import Artist, Release, Track, TrackRelease
 
 class ArtistSearchResult(object):
 
@@ -22,9 +24,10 @@ class ArtistSearchResult(object):
         self.imageUrl = None
         self.tags = []
         self.alternateNames = []
+        self.roadieId = None
 
     def __str__(self):
-        return "Id [" + str(self.id) + "], MusicBrainzId [" + str(self.musicBrainzId) + "], " + \
+        return "Id [" + str(self.id) + "], RoadieId [" + str(self.roadieId) + "], MusicBrainzId [" + str(self.musicBrainzId) + "], " + \
                "AlternateNames [" + str(len(self.alternateNames or [])) + "], Tags [" + str(len(self.tags or [])) + \
                "], ITunesId [" + str(self.iTunesId) + "], AmgId [" + str(self.amgId) + "], Name [" + str(self.name) + "]"
 
@@ -39,9 +42,10 @@ class ArtistReleaseSearchResult(object):
         self.id = None
         self.musicBrainzId = None
         self.iTunesId = None
+        self.roadieId = None
 
     def __str__(self):
-       return "Id [" + str(self.id) + ", MusicBrainzId [" + str(self.musicBrainzId) + "], ITunesId [" + str(self.iTunesId) + \
+       return "Id [" + str(self.id) + ", RoadieId [" + str(self.roadieId) + "], MusicBrainzId [" + str(self.musicBrainzId) + "], ITunesId [" + str(self.iTunesId) + \
               "], ReleaseDate [" + str(self.releaseDate) + "], TrackCount [" + str(self.trackCount) + "], Title [" + str(self.title) + "]"
 
 
@@ -54,23 +58,31 @@ class ArtistSearcher(object):
         if not self.referer or self.referer.startswith("http://localhost"):
             self.referer = "http://github.com/sphildreth/roadie"
         self.logger = Logger()
+        d = os.path.dirname(os.path.realpath(__file__)).split(os.sep)
+        path = os.path.join(os.sep.join(d[:-1]), "settings.json")
+        with open(path, "r") as rf:
+            config = json.load(rf)
+        self.dbName = config['MONGODB_SETTINGS']['DB']
+        self.host = config['MONGODB_SETTINGS']['host']
+        self.mongoClient = connect(self.dbName, host=self.host)
 
     def __enter__(self):
         self.conn = sqlite3.connect(self.databaseFilename)
         c = self.conn.cursor()
-        c.execute("create table if not exists Artists (id INTEGER PRIMARY KEY, musicBrainzId text, iTunesId integer, amgId integer, beginDate text, endDate text, artistType text, imageUrl text, name text, sortName text)")
+        c.execute("create table if not exists Artists (id INTEGER PRIMARY KEY, roadieId text, musicBrainzId text, iTunesId integer, amgId integer, beginDate text, endDate text, artistType text, imageUrl text, name text, sortName text)")
         c.execute("create index if not exists ArtistName on Artists (name)")
         c.execute("create table if not exists ArtistTags (id INTEGER PRIMARY KEY, artistId integer, tag text)")
         c.execute("create index if not exists ArtistTagsArtistId on ArtistTags (artistId)")
         c.execute("create table if not exists ArtistAlternateNames (id INTEGER PRIMARY KEY, artistId integer, name text)")
         c.execute("create index if not exists ArtistAlternateNamesArtistId on ArtistAlternateNames (artistId)")
-        c.execute("create table if not exists Releases (id INTEGER PRIMARY KEY, artistId integer, musicBrainzId text, iTunesId integer, title text, releaseDate text, trackCount integer, coverUrl text)")
+        c.execute("create table if not exists Releases (id INTEGER PRIMARY KEY, artistId integer, roadieId text, musicBrainzId text, iTunesId integer, title text, releaseDate text, trackCount integer, coverUrl text)")
         c.execute("create index if not exists ReleasesArtistId on Releases (artistId)")
         self.conn.commit()
         return self
 
     def __exit__(self, exc_type, exc_val, exc_tb):
         self.conn.close()
+        self.mongoClient.close()
 
     def __getArtistFromDB(self,name):
         c = self.conn.cursor()
@@ -80,18 +92,27 @@ class ArtistSearcher(object):
         if r:
             re = ArtistSearchResult(r[8])
             re.id = r[0]
-            re.musicBrainzId = r[1]
-            re.iTunesId = r[2]
-            re.amgId = r[3]
-            re.sortName = r[9]
-            re.beginDate = r[4]
-            re.endDate = r[5]
-            re.artistType = r[6]
-            re.imageUrl = r[7]
-
-            re.tags = None
-            re.alternateNames = None
-
+            re.roadieId = r[1]
+            re.musicBrainzId = r[2]
+            re.iTunesId = r[3]
+            re.amgId = r[4]
+            re.sortName = r[10]
+            re.beginDate = r[5]
+            re.endDate = r[6]
+            re.artistType = r[7]
+            re.imageUrl = r[8]
+            sql = 'SELECT * FROM ArtistTags WHERE artistId = ' + str(re.id)
+            c.execute(sql)
+            rs = c.fetchall()
+            re.tags = []
+            for r in rs:
+                re.tags.append(r[2])
+            sql = 'SELECT * FROM ArtistAlternateNames WHERE artistId = ' + str(re.id)
+            c.execute(sql)
+            rs = c.fetchall()
+            re.alternateNames = []
+            for r in rs:
+                re.alternateNames.append(r[2])
             return re
         else:
             return None
@@ -101,10 +122,11 @@ class ArtistSearcher(object):
         mbId = "null"
         if artistSearchResult.musicBrainzId:
             mbId = '"' + artistSearchResult.musicBrainzId + '"'
-        sql = 'INSERT INTO Artists (musicBrainzId, iTunesId, amgId, beginDate, endDate, artistType, imageUrl, name, sortName) VALUES (' + \
+        sql = 'INSERT INTO Artists (roadieId, musicBrainzId, iTunesId, amgId, beginDate, endDate, artistType, imageUrl, name, sortName) VALUES ("' + \
+              str(artistSearchResult.roadieId) + '",' + \
               mbId + ',' + \
               str(artistSearchResult.iTunesId) + ',' + \
-              str(artistSearchResult.amgId) + ',"' + \
+              str(artistSearchResult.amgId or 0) + ',"' + \
               str(artistSearchResult.beginDate) + '","' + \
               str(artistSearchResult.endDate) + '","' + \
               str(artistSearchResult.artistType) + '","' + \
@@ -119,7 +141,7 @@ class ArtistSearcher(object):
         if artistSearchResult.tags:
             for tag in artistSearchResult.tags:
                 if tag:
-                    sql = 'INSERT INTO ArtistTags (artistId, tag) VALUES (' + str(artistSearchResult.id) + ',"' + str(tag) + ',")'
+                    sql = 'INSERT INTO ArtistTags (artistId, tag) VALUES (' + str(artistSearchResult.id) + ',"' + str(tag) + '")'
                     c.execute(sql)
         self.conn.commit()
 
@@ -128,7 +150,7 @@ class ArtistSearcher(object):
         if artistSearchResult.alternateNames:
             for alternateName in artistSearchResult.alternateNames:
                 if alternateName:
-                    sql = 'INSERT INTO ArtistAlternateNames (artistId, name) VALUES (' + str(artistSearchResult.id) + ',"' + str(alternateName) + ',")'
+                    sql = 'INSERT INTO ArtistAlternateNames (artistId, name) VALUES (' + str(artistSearchResult.id) + ',"' + str(alternateName) + '")'
                     c.execute(sql)
         self.conn.commit()
 
@@ -140,19 +162,42 @@ class ArtistSearcher(object):
         rs = c.fetchall()
         releases = []
         for r in rs:
-            release = ArtistReleaseSearchResult(r[4], r[5], r[6], r[7])
+            release = ArtistReleaseSearchResult(r[5], r[6], r[7], r[8])
             release.id = r[0]
-            release.musicBrainzId = r[2]
-            release.iTunesId = r[3]
+            release.roadieId = r[2]
+            release.musicBrainzId = r[3]
+            release.iTunesId = r[4]
             releases.append(release)
         return releases
 
+    def __markArtistFoundInRoadie(self,artistSearchResult):
+        artist = Artist.objects(__raw__= {'$or' : [
+            { 'Name' : { '$regex' : artistSearchResult.name, '$options': 'mi' }},
+            { 'SortName': { '$regex' : artistSearchResult.name, '$options': 'mi' } },
+            { 'AlternateNames': { '$regex' : artistSearchResult.name, '$options': 'mi' } }
+        ]}).first()
+        if artist:
+            artistSearchResult.roadieId = artist.id
+        return artistSearchResult
+
+    def __markReleasesFoundInRoadie(self, artistSearchResult, artistReleaseSearchResults):
+        if artistSearchResult.roadieId:
+            artist = Artist.objects(id=artistSearchResult.roadieId).first()
+            for r in artistReleaseSearchResults:
+                release = Release.objects(__raw__= {'$or' : [
+                        { 'Title' : { '$regex' : r.title, '$options': 'mi' }},
+                        { 'AlternateNames': { '$regex' : r.title, '$options': 'mi' } }
+                    ]}).filter(Artist = artist).first()
+                if release:
+                    r.roadieId = release.id
+        return artistReleaseSearchResults
 
     def __saveReleasesForArtistToDB(self,artistSearchResult, ArtistReleaseSearchResults):
         c = self.conn.cursor()
         for artistReleaseSearchResult in ArtistReleaseSearchResults:
-            sql = 'INSERT INTO Releases (artistId, musicBrainzId, iTunesId, title, releaseDate, trackCount, coverUrl) VALUES (' + \
+            sql = 'INSERT INTO Releases (artistId, roadieId, musicBrainzId, iTunesId, title, releaseDate, trackCount, coverUrl) VALUES (' + \
                   str(artistSearchResult.id) + ',"' + \
+                  str(artistReleaseSearchResult.roadieId) + '","' +  \
                   str(artistReleaseSearchResult.musicBrainzId) + '",' +  \
                   str(artistReleaseSearchResult.iTunesId) + ',"' +  \
                   str(artistReleaseSearchResult.title) + '","' + \
@@ -179,6 +224,8 @@ class ArtistSearcher(object):
                 artistSearchResult.name = itArtist.name
                 artistSearchResult.iTunesId = itArtist.iTunesId
                 artistSearchResult.amgId = itArtist.amgId
+            if artistSearchResult:
+                artistSearchResult = self.__markArtistFoundInRoadie(artistSearchResult)
             self.__saveArtistToDB(artistSearchResult)
             artistSearchResult = self.__getArtistFromDB(name)
             if mbArtist:
@@ -210,14 +257,14 @@ class ArtistSearcher(object):
                 if 'tag-list' in mbArtist:
                     for tag in mbArtist['tag-list']:
                         if tag:
-                            t = string.capwords(tag['name'][:-1])
+                            t = string.capwords(tag['name'])
                             if not t in artistSearchResult.tags:
                                 artistSearchResult.tags.append(t)
                 artistSearchResult.alternateNames = []
                 if 'alias-list' in mbArtist:
                     for a in mbArtist['alias-list']:
                         if a and 'alias' in a:
-                            an = string.capwords(a['alias'][:-1])
+                            an = string.capwords(a['alias'])
                             if not an in artistSearchResult.alternateNames:
                                 artistSearchResult.alternateNames.append(an)
             print(artistSearchResult)
@@ -257,6 +304,7 @@ class ArtistSearcher(object):
         if not albumsSearchResult:
             albumsSearchResult = self.__iTunesAlbumsForArtist(artistSearchResult, titleFilter)
             if albumsSearchResult:
+                albumsSearchResult = self.__markReleasesFoundInRoadie(artistSearchResult, albumsSearchResult)
                 self.__saveReleasesForArtistToDB(artistSearchResult, albumsSearchResult)
         if titleFilter:
             for a in albumsSearchResult:
@@ -264,6 +312,7 @@ class ArtistSearcher(object):
                     albumsSearchResult = []
                     albumsSearchResult.append(a)
                     continue
+
         return albumsSearchResult
 
 
